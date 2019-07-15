e6e573be7f3a8d91629a39d86f9adec2d072bcf5
Alexandre Dumont aka Adlx <adumont@users.noreply.github.com>
2019-07-15 11:40:46 +0200

    Add some Repos to vm post prov script

--- vm_post_prov.sh
+++ vm_post_prov.sh
@@ -1,3 +1,13 @@
 # This will run as root
+
+# introduction-to-reinforcement-learning, webinar from O'Reilly
 su jupyter -c "cd ; git clone https://resources.oreilly.com/live-training/introduction-to-reinforcement-learning.git"
+
+# Jon Krohn course on O'Reilly
 su jupyter -c "cd ; git clone https://github.com/the-deep-learners/TensorFlow-LiveLessons.git"
+
+# DeepSchool.io repo
+su jupyter -c "cd ; git clone https://github.com/sachinruk/deepschool.io.git"
+
+# Machine Learning and Deep Learning in Python using Scikit-Learn, Keras and TensorFlow 2 by Aurélien Geron, O'Reilly
+su jupyter -c "cd ; git clone https://github.com/ageron/handson-ml2.git"
