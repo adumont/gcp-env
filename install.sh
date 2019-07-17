165a7127649fd881095034a1444283ae1b59717b
Alexandre Dumont aka Adlx <adumont@users.noreply.github.com>
2019-07-17 13:40:21 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# curl https://raw.githubusercontent.com/adumont/gcp-env/master/install.sh | bash
+
+cd
+
+if [ -d gcp-env ]
+then
+  echo "Already installed? Skipping"
+  exit 1
+fi
+
+# clone this repo
+git clone https://github.com/adumont/gcp-env.git
+
+# add the bashrc stuff to user's bashrc
+cat gcp-env/bashrc >> ~/.bashrc
