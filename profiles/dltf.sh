e9e854f40563a57d00302f03c882b29108034b75
Alexandre Dumont aka Adlx <adumont@users.noreply.github.com>
2019-07-18 14:07:17 +0200

    Update dltf.sh

--- profiles/dltf.sh
+++ profiles/dltf.sh
@@ -13,3 +13,15 @@ su jupyter -c "cd ; git clone https://github.com/ageron/handson-ml2.git"
 # Jon Krohn course on O'Reilly
 su jupyter -c "cd ; git clone https://github.com/the-deep-learners/TensorFlow-LiveLessons.git"
 
+# Requirements, see https://github.com/the-deep-learners/TensorFlow-LiveLessons/blob/master/Dockerfile
+
+# install tflearn and keras: 
+pip install tflearn==0.3.2
+pip install keras==2.0.8
+
+# install NLP packages:
+pip install nltk==3.2.4
+pip install gensim==2.3.0
+
+# install Reinforcement Learning packages:
+pip install gym==0.9.4
