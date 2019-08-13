272e166550754d14a4b7891adc85f12b81995817
Alexandre Dumont <adumont@gmail.com>
2019-08-13 21:15:29 +0200

    Installs .gitconfig in gcloud shell env

--- install.sh
+++ install.sh
@@ -15,3 +15,6 @@ git clone https://github.com/adumont/gcp-env.git
 # add the bashrc stuff to user's bashrc
 [ -e gcp-env/bashrc ] && cat gcp-env/bashrc >> ~/.bashrc
 source .bashrc
+
+# add my .gitconfig
+cd ; wget https://gist.githubusercontent.com/adumont/1380188/raw/.gitconfig
