51ff4aefc0c0bbabff00fa4a667af2b215d64b47
Alexandre Dumont <adumont@gmail.com>
2019-08-14 19:54:44 +0200

    run run-after-git-clone

--- profiles/fastai.sh
+++ profiles/fastai.sh
@@ -16,7 +16,7 @@ su jupyter -c "cd ; git clone https://github.com/ageron/handson-ml2.git"
 su jupyter -c "wget https://gist.githubusercontent.com/adumont/1380188/raw/.gitconfig -O ~/.gitconfig"
 
 # My notes & notebooks for ML & DL, by @adumont
-su jupyter -c "cd ; git clone https://github.com/adumont/ml-dl-notes.git"
+su jupyter -c "cd ; git clone https://github.com/adumont/ml-dl-notes.git; cd ml-dl-notes/; tools/run-after-git-clone"
 
 # Link to fastai notebooks in home dir
 su jupyter -c "cd ; ln -s tutorials/fastai/course-v3/nbs fastai"
